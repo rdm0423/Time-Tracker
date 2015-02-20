c9446378596577b418f614e3b00ebe4928717c1b
Ross McIlwaine <rdm0423@gmail.com>
2015-02-20 15:26:03 -0700

    Step 9: Add CustomEntryViewController
    Including xib file

---
+++ Time-Tracker/CustomEntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomEntryViewController.h
+//  Time-Tracker
+//
+//  Created by Ross McIlwaine on 2/20/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomEntryViewController : UIViewController
+
+@end
